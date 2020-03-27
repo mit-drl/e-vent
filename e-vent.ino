fc2b19ab42bcddd3a2e03de3c351c01c5a9a5097
br.araki <br.araki@gmail.com>
2020-03-27 13:06:08 -0400

    made 2 minor changes to make it compile on my old mac

--- e-vent.ino
+++ e-vent.ino
@@ -1,3 +1,5 @@
+enum States {DEBUG_STATE, IN_STATE, PAUSE_STATE, EX_STATE};
+
 #include <LiquidCrystal.h>
 #include <RoboClaw.h>
 #include "Display.h"
@@ -40,7 +42,6 @@ float VOL_MIN = 100;
 float VOL_MAX = 450; // 900; // For full 
 
 //Setup States
-enum States {DEBUG_STATE, IN_STATE, PAUSE_STATE, EX_STATE};
 States state;
 bool enteringState;
 unsigned long stateTimer;
@@ -172,7 +173,7 @@ void setup() {
 void loop() {
   if(DEBUG){
     if(Serial.available() > 0){
-      setState(Serial.parseInt());
+      setState((States) Serial.parseInt());
       while(Serial.available() > 0) Serial.read();
     }
   }
