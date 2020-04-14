a329931dfba14bb67b80bf2e0ac488f4ea50779b
abukhalaf <murad@mit.edu>
2020-04-14 14:31:43 -0400

    fix constant name

--- e-vent.ino
+++ e-vent.ino
@@ -120,7 +120,7 @@ int motorPosition = 0;
 // LCD Screen
 const int rs = 9, en = 8, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-display::Display displ(&lcd, TRIGGER_LOWER_THRESHOLD);
+display::Display displ(&lcd, TRIGGERSENSITIVITY_OFF);
 
 // Alarms
 alarms::AlarmManager alarm(BEEPER_PIN, SNOOZE_PIN, &displ, &cycleCount);
