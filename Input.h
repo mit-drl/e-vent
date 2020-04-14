7d87f248a2858645e3aa20d605b9fccc375b981d
Amado Antonini <amadoantonini@gmail.com>
2020-04-13 23:09:57 -0400

    move definitions to cpp

--- Input.h
+++ Input.h
@@ -22,21 +22,13 @@ using display::Display;
 template <typename T>
 class Input {
 public:
-  Input(Display* displ, const display::DisplayKey& key): 
-      displ_(displ),
-      disp_key_(key) {}
+  Input(Display* displ, const display::DisplayKey& key): displ_(displ), disp_key_(key) {}
 
-  void begin(T (*read_fun)()) {
-    read_fun_ = read_fun;
-    // don't require confirmation the on the very first reading
-    set_value_ = read_fun_();
-  }
+  void begin(T (*read_fun)());
 
   virtual void update() = 0;
 
-  T read() const {
-    return set_value_;
-  }
+  inline T read() const { return set_value_; }
 
 protected:
   T (*read_fun_)();
@@ -45,14 +37,7 @@ protected:
 
   T set_value_;  // Dial value displayed and used for operation
 
-  void display(const T& value, const bool& blank = false) {
-    if (blank) {
-      displ_->writeBlank(disp_key_);
-    }
-    else {
-      displ_->write(disp_key_, value);
-    }
-  }
+  void display(const T& value, const bool& blank = false);
 
   inline String toString(const T& val) const { return displ_->toString(disp_key_, val); }
 
@@ -69,10 +54,7 @@ class Knob : public Input<T> {
 public:
   Knob(Display* displ, const display::DisplayKey& key): Input<T>(displ, key) {}
 
-  void update() {
-    this->set_value_ = read_fun_();
-    this->display(read());  
-  }
+  void update();
 };
 
 
@@ -95,35 +77,9 @@ public:
       alarms_(alarms),
       pulse_(1000, 0.5) {}
 
-  void begin(T (*read_fun)()) {
-    Input<T>::begin(read_fun);
-    confirm_button_.begin();
-  }
+  void begin(T (*read_fun)());
 
-  void update() {
-    unconfirmed_value_ = read_fun_();
-    if (!isSignificant(unconfirmed_value_ - this->set_value_)) {
-      this->display(read());
-      if (!confirmed_) {
-        confirmed_ = true;
-        alarms_->unconfirmedChange(false);
-      }
-    }
-    else if (confirm_button_.is_LOW()) {
-      this->set_value_ = unconfirmed_value_;
-    }
-    else {
-      const unsigned long time_now = millis();
-      if (confirmed_) {
-        time_changed_ = time_now;
-        confirmed_ = false;
-      }
-      this->display(unconfirmed_value_, !pulse_.read());
-      if (time_now - time_changed_ > kAlarmTime) {
-        alarms_->unconfirmedChange(true, getConfirmPrompt());
-      }
-    }
-  }
+  void update();
 
 private:
   utilities::DebouncedButton confirm_button_;
@@ -133,18 +89,18 @@ private:
   unsigned long time_changed_ = 0;
   bool confirmed_ = true;
 
-  String getConfirmPrompt() const {
-    char buff[display::kWidth + 1];
-    sprintf(buff, "Set %s(%s)->%s?", this->getLabel().c_str(), 
-            this->toString(this->set_value_).c_str(), this->toString(unconfirmed_value_).c_str());
-    String s(buff);
-    while (s.length() < display::kWidth) s += " ";
-    return s;
-  }
+  String getConfirmPrompt() const;
 
   // Check if a setting change is big enough to request confirmation
   inline bool isSignificant(const T& change) { return abs(change) > 0.2; }
 };
+
+
+// Instantiation of template classes
+template class Knob<int>;
+template class Knob<float>;
+template class SafeKnob<int>;
+template class SafeKnob<float>;
 
 
 }
