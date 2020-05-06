e5286cf4501ea58fcaeb0cd2051117449c81f9a4
Teddy Ort <teddy@mit.edu>
2020-05-05 22:32:31 -0400

    Formatting

--- Buttons.h
+++ Buttons.h
@@ -1,11 +1,9 @@
-
 /**
  * MIT Emergency Ventilator Controller
  * 
  * MIT License:
  * 
- 
-* Copyright (c) 2020 MIT
+ * Copyright (c) 2020 MIT
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
