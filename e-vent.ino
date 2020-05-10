eb4483bca4c08a31c79dea248454c4daa7fdc363
abukhalaf <murad@mit.edu>
2020-05-10 11:18:30 -0400

    Enable AC for 2.0 knob setting

--- e-vent.ino
+++ e-vent.ino
@@ -256,7 +256,7 @@ void loop() {
 
       // Check if patient triggers inhale
       patientTriggered = pressureReader.get() < (pressureReader.peep() - knobs.ac()) 
-          && knobs.ac() > AC_MIN;
+          && (knobs.ac() > AC_MIN - 1e-2f);
 
       if (patientTriggered || now() - tCycleTimer > tPeriod) {
         if (!patientTriggered) pressureReader.set_peep();  // Set peep again if time triggered
